bf1b3c032e0347feaf7dec37f4ba107271e4761a
Drew Steen <andrew.decker.steen@gmail.com>
2013-11-28 18:21:31 -0500

    read_plate_setup and sample_name_parser seem to work

--- R/read_plate_setup.R
+++ R/read_plate_setup.R
@@ -3,22 +3,23 @@
 ##' @description For now: takes a filename, reads the file and melts it
 ##' @export
 
-read_plate_setup <- function(fn="data/sample_plate_layout.csv") {
+read_plate_setup <- function(fn="data/sample_plate_layout.csv", ncol=13) {
   
   # Read plate setup worksheet
   d <- read.csv(fn)
   first_col <- names(d)[1]
   
   # Melt df & create column of plate IDs
-  dm <- melt(d, id.vars=first_col)
+  dm <- melt(d, id.vars=first_col, value.name="sample.name")
   dm$well <- toupper(paste(dm$variable, dm[, first_col], sep=""))
   #dm_skinny <- subset(dm, select="well", "value")
-  dm_skinny <- dm[ , c("well", "value")]
+  dm_skinny <- dm[ , c("well", "sample.name")]
   
   # Parse sample names
    # I guess I need to write sample_name_parser as vectorized 
   d_parsed <- sample_name_parser(dm_skinny)
   
+  browser()
   d_parsed
   
 }
--- R/sample_name_parser.R
+++ R/sample_name_parser.R
@@ -2,7 +2,9 @@
 ##' 
 ##' @param names_vec A vector of names
 
-sample_name_parser <- function(d, sample_name_col="value", col_names=c("std.sample", "conc", "fluorophore", "buffer.sample")) {
+sample_name_parser <- function(d, sample_name_col="sample.name", col_names=c("std.sample", "conc", "fluorophore", "buffer.sample")) {
+  # d is meant to be a melted data frame of plate addresses and the name of the sample in the plate
+  # d should have two columns: one with plate addresses (e.g. C6) and one with the text name of the sample (e.g. std, 50 uM, MUB)
   
   #####
   # Strategy 1
@@ -12,8 +14,11 @@ sample_name_parser <- function(d, sample_name_col="value", col_names=c("std.samp
   
   names_list <- strsplit(names_vec, split=", ") # replace with regular expression that will recognize multiple spaces
   parsed_df <- ldply(names_list, identity)
+  
+  
   sample_identifier <- cbind(d, parsed_df)
   
+  #browser()
   sample_identifier
   
 }
