1932f1ebb7b40b38f1879cc3b3cd1a3e944fb6d7
Drew Steen <andrew.decker.steen@gmail.com>
2014-02-28 15:43:26 -0500

    Added function tidy_biotek

---
+++ R/tidy_biotek.R
@@ -0,0 +1,15 @@
+##' Puts biotek files into tidy form
+##'
+##' @description Formats raw biotek files into long form with standardized column headings
+##' 
+
+tidy_biotek <- function(d, ...) {
+  
+  # Note: Fictional experiment: 
+  # 16 standards: 0:15 uM (columns 1:16), 3 reps each (48 total)
+  # 2 substrates (A-AMC, B-AMC, C-AMC, etc.)
+  # 3 live reps and 1 control rep for each substrate
+  # 10 concentrations for each substrate
+  
+  
+}
