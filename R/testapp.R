658c7021f0b8faa3c9f303fd7c151cd27157a57f
joshgurka <jgurka@vols.utk.edu>
2015-07-22 15:34:45 -0400

    rudimentary functionality
    Good basis; build from here and slowly start implementing pieces of the
    find_activity function

--- R/testapp.R
+++ R/testapp.R
@@ -9,10 +9,14 @@ ui <- fluidPage(
       fileInput(inputId = "data", label = "Data"),
       helpText("Select the desired read.csv parameters"),
       checkboxInput(inputId = 'header', label = 'Header', value = TRUE),
-      checkboxInput(inputId = "stringAsFactors", label = "stringAsFactors", value = FALSE)
+      checkboxInput(inputId = "stringAsFactors", label = "stringAsFactors", value = FALSE),
+      radioButtons(inputId = 'sep', label = 'Separator', 
+                   choices = c(Comma=',', Semicolon=';', Tab='\t', Space=''),
+                   selected = ',')
     ),
     mainPanel(
       # uiOutput("names"), DELETE
+      uiOutput("uncal"),
       uiOutput("designvars"),
       uiOutput("timevar"),
       uiOutput("fluorescence")
@@ -23,12 +27,15 @@ ui <- fluidPage(
       fileInput(inputId = "curve", label = "Calibration Curve"),
       helpText("Select the desired read.csv parameters"),
       checkboxInput(inputId = 'header2', label = 'Header', value = TRUE),
-      checkboxInput(inputId = "stringAsFactors2", label = "stringAsFactors", value = FALSE)
+      checkboxInput(inputId = "stringAsFactors2", label = "stringAsFactors", value = FALSE),
+      radioButtons(inputId = 'sep2', label = 'Separator', 
+                   choices = c(Comma=',', Semicolon=';', Tab='\t', Space=''),
+                   selected = ',')
     ),
-    
     mainPanel(
-      uiOutput("concentration.variable"),
-      uiOutput("fluorescence.variable2")
+      uiOutput("concentration"),
+      uiOutput("curvefluorescence"),
+      uiOutput("cal")
     )
   )
 )
@@ -44,28 +51,26 @@ server <- function(input, output){
 #     head(d_uncal)
 #   })
   
+  # Read the Raw Data and extract the names of the columns 
   nms <- reactive({
     file1 <- input$data
     if(is.null(file1)){return()} 
     d_uncal <- read.csv(file = file1$datapath, 
-                        header = input$header, 
-                        stringsAsFactors = input$stringsAsFactors
-                        )
+                        header = TRUE, stringsAsFactors = FALSE)
     nm <- names(d_uncal)
     nm
   })
   
+  # Read the Calibration Curve Data and extract the column names
   nms2 <- reactive({
-    file2 <- input$curve
-    if(is.null(file2)){return()} 
-    d_cal <- read.csv(file = file2$datapath, 
-                      header = input$header2, 
-                      stringsAsFactors = input$stringsAsFactors2
-                      )
-    nm2 <- names(d_cal)
-    nm2
+    file1 <- input$curve
+    if(is.null(file1)){return()} 
+    d_uncal <- read.csv(file = file1$datapath, 
+                        header = input$header2, 
+                        stringsAsFactors = FALSE)
+    nm <- names(d_uncal)
+    nm
   })
-  
   
 #  Commented out until Im done and clean up old code
 #   output$names <- renderPrint({
@@ -76,6 +81,7 @@ server <- function(input, output){
 #     
 #   })
   
+  # Uses extracted column names from the raw data as options in a checkbox selector
   output$designvars <- renderUI({
     if(is.null(nms()))
       return()
@@ -83,6 +89,7 @@ server <- function(input, output){
     checkboxGroupInput("design.variables", "Select Design Variables", nms())
   })
   
+  # Uses extracted column names from the raw data as options to select the Time Variable
   output$timevar <- renderUI({
     if(is.null(nms()))
       return()
@@ -90,6 +97,7 @@ server <- function(input, output){
       radioButtons("time.variable", "Select Time Variable", nms())
   })
   
+   # Uses extracted column names from the raw data as options to select the fluorescence variable
   output$fluorescence <- renderUI({
     if(is.null(nms()))
       return()
@@ -97,38 +105,88 @@ server <- function(input, output){
       radioButtons("fluorescence.variable", "Select Fluorescence Variable", nms())
     })
   
-  data <- reactive({
-    file1 <- input$data
-    if(is.null(file1)){return()} 
-    d_uncal <- read.csv(file = file1$datapath,
-                        header = input$header, 
-                        stringsAsFactors = input$stringsAsFactors
-                        )
-    head(d_uncal)
-  })
-  
-  curve <- reactive({
-    file2 <- input$curve
-    if(is.null(file2)){return()} 
-    d_cal <- read.csv(file = file2$datapath,
-                      header = input$header2,
-                      stringsAsFactors = input$stringAsFactors2)
-    head(d_cal)
-  })
-  
-  output$conc <- renderUI({
+  # Uses extracted column names from the calibration curve as options to select the concentration variable
+  output$concentration <- renderUI({
     if(is.null(nms2()))
       return()
     else
       radioButtons("concentration.variable", "Select Concentration Variable", nms2())
   })
   
-  output$fluorescence2 <- renderUI({
+  # Uses extracted column names from the calibration curve as options to select the fluorescence variable
+  output$curvefluorescence <- renderUI({
     if(is.null(nms2()))
       return()
     else
-      radioButtons("fluorescence.variable2", "Select Fluorescence Variable", nms2())
+      radioButtons("fluorescence.curve", "Select Fluorescence Variable", nms2())
   })
+  
+  # First step of find_activity; read .csv file
+  # Returns head of data frame as checkpoint for this step
+  data <- reactive({
+    file1 <- input$data
+    if(is.null(file1)){return()} 
+    d_uncal <- read.csv(file = file1$datapath,
+                        header = input$header, 
+                        stringsAsFactors = FALSE)
+    
+    head(d_uncal)
+  })
+  
+  # Creates tab about the data file
+  output$file <- renderTable({
+    if(is.null(data())){return ()}
+    input$data
+  })
+  
+  # Creates tab to display dataframe
+  output$table <- renderTable({
+    if(is.null(data())){return ()}
+    data()
+  })
+  
+  # Creates the UI output including all tabs about the raw data
+  output$uncal <- renderUI({
+    if(is.null(data()))
+      return()
+    else
+      tabsetPanel(tabPanel("About file", tableOutput("file")),
+                  tabPanel("Uploaded Data", tableOutput("table")))
+  })
+  
+  # used to carry out find_activty process for the calibration curve data
+  # Returns head of data frame as checkpoint for this step
+  data2 <- reactive({
+    file1 <- input$curve
+    if(is.null(file1)){return()} 
+    d_uncal <- read.csv(file = file1$datapath,
+                        header = input$header2, 
+                        stringsAsFactors = FALSE)
+    
+    head(d_uncal)
+  })
+  
+  # Creates tab about the calibration curve file
+  output$file2 <- renderTable({
+    if(is.null(data2())){return ()}
+    input$curve
+  })
+  
+  # Creates tab to display calibration curve data frame
+  output$table2 <- renderTable({
+    if(is.null(data2())){return ()}
+    data2()
+  })
+  
+  # Creates the UI output including all tabs about the calibration curve
+  output$cal <- renderUI({
+    if(is.null(data2()))
+      return()
+    else
+      tabsetPanel(tabPanel("About file", tableOutput("file2")), 
+                  tabPanel("Curve", tableOutput("table2")))
+  })
+  
 }
 
 shinyApp(ui = ui, server = server)
