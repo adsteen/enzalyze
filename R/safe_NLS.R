83cd0c9ceb318315877eb1a9c930ea0db09abf17
adsteen <andrew.decker.steen@gmail.com>
2017-08-14 17:07:53 -0400

    now works, formerly didn't

--- R/safe_NLS.R
+++ R/safe_NLS.R
@@ -3,7 +3,7 @@
 
 #safe_NLS <- function(df, xvar="conc", yvar="v0", form=NULL, guesses=NULL) {
 safe_NLS <- function(df, xcol="conc", ycol="v0", form=NULL, guesses=NULL) {
-  browser()
+  #browser()
   # Create a default model based on Shane & Katherine's data
   if(is.null(form)) {
     form <- formula(I(v0 ~ (Vmax * conc)/(Km + conc)))
@@ -17,13 +17,12 @@ safe_NLS <- function(df, xcol="conc", ycol="v0", form=NULL, guesses=NULL) {
     # First tier is LW-Burke approach, 2nd tier is this (or could use nls2 and try 'em both)
     
     # Need to include some code to ensure that the guesses have the same variables as the formula
-    # Km_guess <- mean(xvals, na.rm=TRUE)
     Km_guess <- mean(df[ , xcol], na.rm=TRUE)
-    #Vmax_guess <- max(yvals, na.rm=TRUE)
     Vmax_guess <- max(df[ , ycol], na.rm=TRUE)
     guesses <- list(Km=Km_guess, Vmax=Vmax_guess)
   }
   
+  # Create model, return NULL with warning if it fails
   mod <- tryCatch(
     nls2(form, df, start=guesses),
     # Note: on warning, the function executes and the warning is issued
