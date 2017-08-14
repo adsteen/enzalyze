99e3325a2d265bfcae720626729fb9320689c8d0
adsteen <andrew.decker.steen@gmail.com>
2017-08-14 17:05:00 -0400

    typo fix

--- R/safe_NLS.R
+++ R/safe_NLS.R
@@ -1,26 +1,31 @@
 # Calculate NLS & return
 # To do: import model as a parameter
 
-safe_NLS <- function(df, xvar="conc", yvar="v0", form=NULL, guesses=NULL) {
-  #modbrowser()
+#safe_NLS <- function(df, xvar="conc", yvar="v0", form=NULL, guesses=NULL) {
+safe_NLS <- function(df, xcol="conc", ycol="v0", form=NULL, guesses=NULL) {
+  browser()
   # Create a default model based on Shane & Katherine's data
   if(is.null(form)) {
-    mod <- formula(I(v0 ~ (Vmax * conc)/(Km + conc)))
+    form <- formula(I(v0 ~ (Vmax * conc)/(Km + conc)))
   }
   
-  xvals <- df[ , xvar]
-  yvals <- df[ , yvar]
+  #xvals <- df[ , xvar]
+  #yvals <- df[ , yvar]
   
   if(is.null(guesses)) {
+    # Note: I could maybe get a better guess with a 2-tiered approach
+    # First tier is LW-Burke approach, 2nd tier is this (or could use nls2 and try 'em both)
+    
     # Need to include some code to ensure that the guesses have the same variables as the formula
-    # warning("Dude you should really make a starting guess, but I'll try to make one for you")
-    Km_guess <- mean(xvals, na.rm=TRUE)
-    Vmax_guess <- max(xvals, na.rm=TRUE)
+    # Km_guess <- mean(xvals, na.rm=TRUE)
+    Km_guess <- mean(df[ , xcol], na.rm=TRUE)
+    #Vmax_guess <- max(yvals, na.rm=TRUE)
+    Vmax_guess <- max(df[ , ycol], na.rm=TRUE)
     guesses <- list(Km=Km_guess, Vmax=Vmax_guess)
   }
   
   mod <- tryCatch(
-    nls(mod, df, start=guesses),
+    nls2(form, df, start=guesses),
     # Note: on warning, the function executes and the warning is issued
     error=function(err) {
       warning("This model threw an error")
