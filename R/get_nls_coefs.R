48b62942f51ff43ab900b93ca8df2d0a1911ee9b
Drew Steen <andrew.decker.steen@gmail.com>
2017-10-06 14:41:11 -0400

    omigod I deleted all the extraneous fxns

--- R/get_nls_coefs.R
+++
@@ -1,8 +0,0 @@
-get_nls_coefs <- function(x) {
-  # x is a nls fit
-  coeffs <- summary(x)$coefficients
-  data.frame(k=coeffs[1, 1],
-             k.se=coeffs[1, 2],
-             recalcitrant=coeffs[2, 1],
-             recalcitrant.se=coeffs[2, 2])
-}
